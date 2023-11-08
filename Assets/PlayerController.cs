89079af93ba45522a19eaf07f63c913aee3c196d
Dante Norin <119510231+InteLoweKlewebeck@users.noreply.github.com>
2023-11-08 10:34:23 +0100

    Fortsatt på platformer

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,19 +6,59 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     float speed;
+
+    [SerializeField]
+    float jumpForce = 100;
+
+    [SerializeField]
+    Transform groundCheck;
+
+    [SerializeField]
+    float groundRadius = 0.1f;
+
+    [SerializeField]
+    LayerMask groundLayer;
+
+    bool mayJump = true;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         float moveX = Input.GetAxisRaw("Horizontal");
-
         Vector2 movementX = new Vector2(moveX, 0);
+        transform.Translate(movementX * speed * Time.deltaTime);
 
-        transform.Translate(movementX * speed * Time.deltaTime);   
+        bool isGrounded = Physics2D.OverlapBox(groundCheck.position, MakeGroundcheckSize(), 0, groundLayer);
+
+        if (Input.GetAxisRaw("Jump") > 0 && mayJump == true && isGrounded == true)
+        {
+            Rigidbody2D rb = GetComponent<Rigidbody2D>();
+            Vector2 jump = Vector2.up * jumpForce;
+            rb.AddForce(jump);
+
+            mayJump = false;
+
+        }
+
+        if (Input.GetAxisRaw("Jump") == 0)
+        {
+            mayJump = true;
+        }
+
+
     }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(groundCheck.position, MakeGroundcheckSize());
+    }
+
+    private Vector3 MakeGroundcheckSize() => new Vector3(1, groundRadius);
 }
