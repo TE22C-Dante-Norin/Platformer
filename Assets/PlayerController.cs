0fc1aeb81439cbcd528e123fbde7b31d56d2bac2
Dante Norin <119510231+InteLoweKlewebeck@users.noreply.github.com>
2023-11-28 15:35:34 +0100

    kljljh

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -40,6 +40,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         float moveX = Input.GetAxisRaw("Horizontal");
         if (moveX > 0f)
         {
